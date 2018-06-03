0ada19693e5bde01a1b6370d117096b43828c3a8
Тимур Бачеев <timur.bacheev@gmail.com>
2018-06-04 02:12:29 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Reads file and shows N most frequent words in it.
+
+# Quickstart
+
+You have to start program with a file as a parameter and number of top words (default 10). See the following example.
+
+Example of script launch on Linux, Python 3.6:
+
+```bash
+
+$ python3 lang_frequency.py -f <path to file> -n <number of words>
+
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,37 @@
+import argparse
 
+NUMBER_OF_WORDS = 10
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as file:
+    	text = file.read().lower().split()
+    return text
 
+def get_most_frequent_words(text, n=NUMBER_OF_WORDS):
+    counter = {}
+    {word: 1 if word not in counter and not counter.update({word: 1})
+                  else counter[word] + 1
+                  if not counter.update({word: counter[word] + 1}) else 1 for word in text}
+    return sorted(counter.items(), key=lambda i: i[1], reverse=True)[:n]
 
-def get_most_frequent_words(text):
-    pass
+def argparser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-f','--filepath', help='full path to text file')
+	parser.add_argument('-n','--number', help='number of words')
+	args = parser.parse_args()
+	return args
 
+def main():
+	args = argparser()
+	if args.filepath is not None:
+		filepath = args.filepath
+	else:
+		filepath = input('Enter full path to text file: ')
+	text = load_data(filepath)
+	if args.number is not None:
+		print(get_most_frequent_words(text, int(args.number)))
+	else:
+		print(get_most_frequent_words(text))
 
 if __name__ == '__main__':
-    pass
+    main()
