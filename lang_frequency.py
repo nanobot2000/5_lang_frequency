bcd444ac2b6774196feffef8eb0931c7490df40f
nanobot2000 <timur.bacheev@gmail.com>
2018-06-04 11:45:54 +0300

    pep8 improvements; counter changed to collections.Counter(); variable names improvements

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,37 +1,44 @@
 import argparse
+from collections import Counter
 
 NUMBER_OF_WORDS = 10
 
+
 def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
-    	text = file.read().lower().split()
+        text = file.read().lower().split()
     return text
 
-def get_most_frequent_words(text, n=NUMBER_OF_WORDS):
-    counter = {}
-    {word: 1 if word not in counter and not counter.update({word: 1})
-                  else counter[word] + 1
-                  if not counter.update({word: counter[word] + 1}) else 1 for word in text}
-    return sorted(counter.items(), key=lambda i: i[1], reverse=True)[:n]
+
+def get_most_frequent_words(text, number=NUMBER_OF_WORDS):
+    return Counter(text).most_common(number)
+
 
 def argparser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-f','--filepath', help='full path to text file')
-	parser.add_argument('-n','--number', help='number of words')
-	args = parser.parse_args()
-	return args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filepath', help='full path to text file')
+    parser.add_argument('-n', '--number', help='number of words')
+    args = parser.parse_args()
+    return args
+
 
 def main():
-	args = argparser()
-	if args.filepath is not None:
-		filepath = args.filepath
-	else:
-		filepath = input('Enter full path to text file: ')
-	text = load_data(filepath)
-	if args.number is not None:
-		print(get_most_frequent_words(text, int(args.number)))
-	else:
-		print(get_most_frequent_words(text))
+    args = argparser()
+    if args.filepath is not None:
+        path = args.filepath
+    else:
+        path = input('Enter full path to text file: ')
+    text = load_data(path)
+    if args.number is not None:
+        most_frequent_words = get_most_frequent_words(text, number=int(args.number))
+    else:
+        most_frequent_words = get_most_frequent_words(text)
+    print("The most popular words:")
+    print("-----------------------")
+    for number, (word, amount) in enumerate(most_frequent_words, 1):
+        print("{} \t {} \t {}".format(number, word, amount))
+    print("_______________________")
+
 
 if __name__ == '__main__':
     main()
