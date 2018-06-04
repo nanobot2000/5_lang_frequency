8c185c54c036874d2c8bc34ceed0519537d10107
nanobot2000 <timur.bacheev@gmail.com>
2018-06-04 17:44:23 +0300

    removed global vars; added restrictions on args; added get_words func to transform text data into list of words

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,23 +1,23 @@
 import argparse
 from collections import Counter
 
-NUMBER_OF_WORDS = 10
-
-
 def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
-        text = file.read().lower().split()
+        text = file.read()
     return text
 
 
-def get_most_frequent_words(text, number=NUMBER_OF_WORDS):
+def get_words(text):
+    return text.lower().split()
+
+def get_most_frequent_words(text, number):
     return Counter(text).most_common(number)
 
 
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filepath', help='full path to text file')
-    parser.add_argument('-n', '--number', help='number of words')
+    parser.add_argument('-f', '--filepath', help='full path to text file', required=True)
+    parser.add_argument('-n', '--number', help='number of words', default=10, type=int)
     args = parser.parse_args()
     return args
 
@@ -29,15 +29,16 @@ def main():
     else:
         path = input('Enter full path to text file: ')
     text = load_data(path)
+    words = get_words(text)
     if args.number is not None:
-        most_frequent_words = get_most_frequent_words(text, number=int(args.number))
+        most_frequent_words = get_most_frequent_words(words, number=args.number)
     else:
-        most_frequent_words = get_most_frequent_words(text)
-    print("The most popular words:")
-    print("-----------------------")
+        most_frequent_words = get_most_frequent_words(words)
+    print('The most popular words:')
+    print('-----------------------')
     for number, (word, amount) in enumerate(most_frequent_words, 1):
-        print("{} \t {} \t {}".format(number, word, amount))
-    print("_______________________")
+        print('{} \t {} \t {}'.format(number, word, amount))
+    print('_______________________')
 
 
 if __name__ == '__main__':
